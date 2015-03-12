496a866ee73c5f1d3e0dbc35d0bd70089d215470
shenlian <shenlian@gmx.de>
2015-03-12 23:33:59 +0100

    do not smooth

--- src/main/java/de/shenlian/BarcodeMonitor/SimpleBarcodeDetector.java
+++ src/main/java/de/shenlian/BarcodeMonitor/SimpleBarcodeDetector.java
@@ -47,7 +47,7 @@ public class SimpleBarcodeDetector {
         while (canvasFrame.isVisible() && (frame = grabber.grab()) != null) {
             cvClearMemStorage(storage);
 
-            cvSmooth(frame, frame, CV_GAUSSIAN, 9, 9, 2, 2);
+//            cvSmooth(frame, frame, CV_GAUSSIAN, 9, 9, 2, 2);
             if (image == null) {
                 image = IplImage.create(frame.width(), frame.height(), IPL_DEPTH_8U, 1);
                 cvCvtColor(frame, image, CV_RGB2GRAY);
